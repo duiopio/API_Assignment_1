1797eda453480002ea4a8ffa978db54ca0d0a582
duiopio <detognifilippo+github@gmail.com>
2023-01-26 09:52:04 +0100

    Second iteration: showing data on the page

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ function getData() {
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
+    displayData(data);
   });
+}
+
+function displayData(posts) {
+  for (const post of posts) {
+    const postContainer = document.createElement("div");
+    const postTitle = document.createElement("h2");
+    const postBody = document.createElement("p");
+
+    postContainer.classList.add("post-container")
+
+    postTitle.textContent = post.title;
+    postBody.textContent = post.body;
+
+    postContainer.appendChild(postTitle);
+    postContainer.appendChild(postBody);
+    document.body.appendChild(postContainer);
+  }
 }
 
 function  setup() {
