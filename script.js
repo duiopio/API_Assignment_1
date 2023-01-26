6d5a40d17ea02b1283c76156bbd8a1e9181c6422
duiopio <detognifilippo+github@gmail.com>
2023-01-26 10:16:18 +0100

    Third iteration (?): the sidebar works but there are scrolling issues;

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const sidebar = document.querySelector("#sidebar");
+const contentView = document.querySelector("#content-view");
+
 function getData() {
   fetch("data.json")
   .then((response) => response.json())
@@ -10,17 +13,27 @@ function getData() {
 function displayData(posts) {
   for (const post of posts) {
     const postContainer = document.createElement("div");
+
+    postContainer.addEventListener("click", () => {
+      console.log("Click");
+      contentView.innerHTML = `
+          <h2>${post.title}</h2>
+          <p>${post.body}</p>
+      `
+  });
+
     const postTitle = document.createElement("h2");
     const postBody = document.createElement("p");
 
-    postContainer.classList.add("post-container")
+    postContainer.classList.add("post-container");
 
     postTitle.textContent = post.title;
     postBody.textContent = post.body;
 
     postContainer.appendChild(postTitle);
     postContainer.appendChild(postBody);
-    document.body.appendChild(postContainer);
+
+    sidebar.appendChild(postContainer);
   }
 }
 
@@ -29,3 +42,19 @@ function  setup() {
 }
 
 setup();
+
+
+
+
+postContainer.addEventListener("pointerdown", function(event) {
+  const detailContainer = document.createElement("div");
+  const detailTitle = document.createElement("h2");
+  const detailBody = document.createElement("p");
+
+  detailTitle = post.title;
+  detailBody = post.body;
+
+  detailContainer.appendChild(detailTitle);
+  detailContainer.appendChild(detailBody);
+  contentView.appendChild(detailContainer);
+});
