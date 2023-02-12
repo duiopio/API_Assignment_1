84870cbe8773ddb6c9854b7550b84a27efb64a86
duiopio <detognifilippo+github@gmail.com>
2023-02-12 17:52:09 +0100

    Updated the code so the user can convert specific amounts.

--- script.js
+++ script.js
@@ -1,101 +1,87 @@
 const sidebar = document.querySelector("#sidebar");
-const contentView = document.querySelector("#content-view");
+const contentArea = document.querySelector("#content");
 let showingComments = false;
 
 function getData() {
-  fetch("https://jsonplaceholder.typicode.com/posts")
+  fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    displayData(data);
+    populateSidebar(data);
   });
 }
 
-function displayData(posts) {
-  for (const post of posts) {
-    const postContainer = document.createElement("div");
-    postContainer.addEventListener("pointerdown", (event) => {
-      showingComments = !showComments;
-      updateContentView(post.title, post.body);
+function populateSidebar(data) {
+  const currencies = Object.entries(data);
+  console.log(currencies);
+  currencies.map((currency) => {
+    const rowElement = document.createElement("div");
+    const currencyName = document.createElement("h2");
+    const currencyCode = document.createElement("h3");
+
+    rowElement.classList.add("sidebar-row");
+
+    rowElement.addEventListener("pointerdown", () => {
+      contentArea.innerHTML = "";
+      const currencyCode = document.createElement("h3");
+      currencyCode.textContent = currency[0];
+
+      getRate("eur", currency[0]);
+
+      contentArea.appendChild(currencyCode);
     });
 
-    const postTitle = document.createElement("h2");
-    const postBody = document.createElement("p");
+    console.log(currency);
+    currencyName.textContent = currency[1];
+    currencyCode.textContent = currency[0];
 
-    postContainer.classList.add("post-container");
+    rowElement.appendChild(currencyName);
+    rowElement.appendChild(currencyCode);
 
-    postTitle.textContent = post.title;
-    postBody.textContent = post.body;
-
-    postContainer.appendChild(postTitle);
-    postContainer.appendChild(postBody);
-
-    sidebar.appendChild(postContainer);
-  }
+    sidebar.appendChild(rowElement);
+  });
 }
 
-function  setup() {
+function getRate(fromCode = "eur", toCode) {
+  fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromCode}/${toCode}.json`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    displayContent(data, fromCode, toCode);
+  });
+}
+
+function displayContent(data, fromCode = "eur", toCode) {
+  const textField = document.createElement("input");
+  textField.type = "number";
+  textField.placeholder = 1;
+  let array = Object.entries(data);
+  let rate = array[1][1];
+  let conversion = rate;
+  
+  textField.addEventListener("input", () => {
+    conversion = (textField.value * rate).toFixed(2);
+
+    if (textField.value != "") {
+      text.textContent = `${textField.value} ${fromCode} is ${conversion} ${toCode}`;
+    } else {
+      conversion = rate.toFixed(2);
+      text.textContent = `1 ${fromCode} is ${conversion} ${toCode}`;
+    }
+  });
+  
+
+  contentArea.innerHTML = "";
+  const text = document.createElement("h2");
+  text.textContent = `1 ${fromCode} is ${conversion} ${toCode}`;
+  contentArea.appendChild(textField);
+  contentArea.appendChild(text);
+}
+
+function setup() {
   getData();
 }
 
+
+
 setup();
-
-function updateContentView(title, body) {
-  contentView.innerHTML = "";
-  const contentTitle = document.createElement("h2");
-  const contentBody = document.createElement("p");
-  const loadCommentsButton = document.createElement("button");
-
-  loadCommentsButton.textContent = "Show comments";
-  loadCommentsButton.addEventListener("pointerdown", () => {
-    getComments(contentView);
-  });
-
-  contentTitle.classList.add("content-title");
-  contentBody.classList.add("content-body");
-
-  contentTitle.textContent = title;
-  contentBody.textContent = body;
-
-  contentView.appendChild(contentTitle);
-  contentView.appendChild(contentBody);
-  contentView.appendChild(loadCommentsButton);
-}
-
-function getComments(content) {
-  fetch("https://jsonplaceholder.typicode.com/comments")
-  .then((response) => response.json())
-  .then((data) => {
-    showComments(data, content);
-  });
-}
-
-function showComments(comments, content) {
-  const commentsWrap = document.createElement("div");
-  commentsWrap.classList.add("comments");
-
-  for (const comment of comments) {
-    const commentContainer = document.createElement("div");
-    const commentTitle = document.createElement("h3");
-    const commentBody = document.createElement("p");
-    const commentEmail = document.createElement("p");
-
-    
-    commentContainer.classList.add("comment-container");
-    commentTitle.classList.add("comment-title");
-    commentBody.classList.add("comment-body");
-    commentEmail.classList.add("comment-email");
-
-    commentTitle.textContent = comment.title;
-    commentBody.textContent = comment.body;
-    commentEmail.textContent = comment.email;
-
-    commentContainer.appendChild(commentTitle);
-    commentContainer.appendChild(commentEmail);
-    commentContainer.appendChild(commentBody);
-
-    commentsWrap.appendChild(commentContainer);
-  }
-
-  content.appendChild(commentsWrap);
-}
